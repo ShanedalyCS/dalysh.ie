7b932d856bd17ad230740ad1af8a9e2d386d9433
ShanedalyCS <shanedaly0903@protonmail.com>
2025-08-15 22:48:30 +0100

    Background Image
    Made a background in photoshop, should turn out pretty cool

---
+++ dalysh.ie/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import '../globals.css'
+
+export default function Navbar () {
+    <>
+    <div className="h1">This is the header</div>
+    </>
+}
---
+++ dalysh.ie/src/pages/Welcome.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+import '../assets/custom.png';
+import Navbar from '../components/Navbar.jsx'
+import '../App.css'
+import '../globals.css'
+
+
+export default function Welcome() {
+
+
+
+  return (
+    <>
+ 
+    <section className='hero'>
+        <div className="landing">
+           <img src="../assets/custom.png" alt='' className='landing-bg'/>
+        </div>
+    </section>
+
+    
+    </>
+    
+  )
+}
