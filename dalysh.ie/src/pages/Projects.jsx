ddf2271639fc6dccbc6425173b59b9ba11fe159f
ShanedalyCS <shanedaly0903@protonmail.com>
2025-08-18 16:50:37 +0100

    project page

--- dalysh.ie/src/pages/Projects.jsx
+++ dalysh.ie/src/pages/Projects.jsx
@@ -1,7 +1,29 @@
-
+import Typewriter from "../components/Typewriter.jsx";
 
 export default function Projects(){
     return(
-        <></>
+        <>
+      
+      <section className="hero">
+        <div className="landing">
+          <div className="hello">
+            {/* <span>{"text before typewriter "}</span> */}
+        <Typewriter
+          text={[
+            "M Y   P R O J E C T S",
+            ":)"
+            
+          ]}
+          speed={70}
+          className="text-white-500"
+          waitTime={1500}
+          deleteSpeed={40}
+          cursorChar={"_"}
+            />
+          </div>
+        </div>
+
+        </section>
+        </>
     )
 }
